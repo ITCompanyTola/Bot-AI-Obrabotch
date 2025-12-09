f4c37d52fd1241a1decd162da7977d4417f8287f
Leezzzzzzyy <vadim.byrsan@mail.ru>
2025-12-09 15:17:43 +0300

    fix merge conflicts

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,5 @@
 export const PRICES = {
   PHOTO_ANIMATION: 80,
-<<<<<<< HEAD
-  MUSIC_CREATION: 165
-=======
-  PHOTO_RESTORATION: 30,
-  MUSIC_CREATION: 25
->>>>>>> feature-photo-restoration
+  MUSIC_CREATION: 165,
+  PHOTO_RESTORATION: 35,
 };

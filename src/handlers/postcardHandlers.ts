38085696c801b120d983b6d6d8a947f943944197
Leezzzzzzyy <vadim.byrsan@mail.ru>
2025-12-30 17:57:14 +0300

    fix: Поменял константу при недостаточном балансе во время генерации открытки по фото

--- src/handlers/postcardHandlers.ts
+++ src/handlers/postcardHandlers.ts
@@ -1,47 +1,63 @@
-import { Telegraf } from 'telegraf';
-import { BotContext, UserState } from '../types';
-import { Database } from '../database';
-import { getPostcardMessage, getPostcardPhotoMessage, POSCTARD_MESSAGE, POSTCARD_MESSAGE_START, POSTCARD_PHOTO_START, POSTCARD_PHOTO_START_WIHOUT, PRICES } from '../constants';
+import { Telegraf } from "telegraf";
+import { BotContext, UserState } from "../types";
+import { Database } from "../database";
+import {
+  getPostcardMessage,
+  getPostcardPhotoMessage,
+  POSCTARD_MESSAGE,
+  POSTCARD_MESSAGE_START,
+  POSTCARD_PHOTO_START,
+  POSTCARD_PHOTO_START_WIHOUT,
+  PRICES,
+} from "../constants";
 
-const HERO_VIDEO_TEXT: string = 'BAACAgIAAxkBAAECdzFpSuGnIPA7Q_WONIwAAZvKW74rJtkAA5YAAnIgWUomnSdhRwQ1VjYE';
-const HERO_PHOTO_VIDEO_ID: string = 'BAACAgIAAxkBAAECaetpSXzBT3SjPpEi5XTEnSVVg5yXJwACU5EAAhKRSUrP-iMveUqEuzYE';
-const EXAMPLE_POSTCARD_PHOTO_ID: string = 'AgACAgIAAxkBAAECXdFpSD25-QLIejlyURmKIPm_QOBbwgACXQ9rG75EQUq_ZhrnMheB_wEAAwIAA3gAAzYE'; // Загрузить и вставить свое фото
-const POSTCARD_PHOTO_INSTRUCTION: string = 'BAACAgIAAxkBAAECdvtpSuAbiBX3l0F_PXF48nyZA1-HcQAC0JUAAnIgWUrN8eIy-x0nKzYE'; // Загрузить и вставить свое видео
-const POSTCARD_TEXT_INSTRUCTION: string = 'BAACAgIAAxkBAAECdvhpSt_r7bS5WGoo7pw1oGNJ4dfUygACy5UAAnIgWUrMQ6MLuolkAzYE';
+const HERO_VIDEO_TEXT: string =
+  "BAACAgIAAxkBAAECdzFpSuGnIPA7Q_WONIwAAZvKW74rJtkAA5YAAnIgWUomnSdhRwQ1VjYE";
+const HERO_PHOTO_VIDEO_ID: string =
+  "BAACAgIAAxkBAAECaetpSXzBT3SjPpEi5XTEnSVVg5yXJwACU5EAAhKRSUrP-iMveUqEuzYE";
+const EXAMPLE_POSTCARD_PHOTO_ID: string =
+  "AgACAgIAAxkBAAECXdFpSD25-QLIejlyURmKIPm_QOBbwgACXQ9rG75EQUq_ZhrnMheB_wEAAwIAA3gAAzYE"; // Загрузить и вставить свое фото
+const POSTCARD_PHOTO_INSTRUCTION: string =
+  "BAACAgIAAxkBAAECdvtpSuAbiBX3l0F_PXF48nyZA1-HcQAC0JUAAnIgWUrN8eIy-x0nKzYE"; // Загрузить и вставить свое видео
+const POSTCARD_TEXT_INSTRUCTION: string =
+  "BAACAgIAAxkBAAECdvhpSt_r7bS5WGoo7pw1oGNJ4dfUygACy5UAAnIgWUrMQ6MLuolkAzYE";
 
-export function registerPostcardHandlers(bot: Telegraf<BotContext>, userStates: Map<number, UserState>) {
-  bot.action('postcard', async (ctx) => {
+export function registerPostcardHandlers(
+  bot: Telegraf<BotContext>,
+  userStates: Map<number, UserState>
+) {
+  bot.action("postcard", async (ctx) => {
     try {
       await ctx.answerCbQuery();
     } catch (error: any) {
-      if (!error.description?.includes('query is too old')) {
-        console.error('Ошибка answerCbQuery:', error.message);
+      if (!error.description?.includes("query is too old")) {
+        console.error("Ошибка answerCbQuery:", error.message);
       }
     }
-    
+
     const userId = ctx.from?.id;
     if (!userId) return;
 
     const balance = await Database.getUserBalance(userId);
     const postcardMessage = POSCTARD_MESSAGE;
     await ctx.telegram.sendMessage(userId, postcardMessage, {
-      parse_mode: 'HTML',
+      parse_mode: "HTML",
       reply_markup: {
         inline_keyboard: [
-          [{text: '💌 Открытка из текста', callback_data: 'postcard_text'}],
-          [{text: '🏞 Открытка из фото', callback_data: 'postcard_photo'}],
-          [{text: 'Главное меню', callback_data: 'main_menu'}],
-        ]
-      }
+          [{ text: "💌 Открытка из текста", callback_data: "postcard_text" }],
+          [{ text: "🏞 Открытка из фото", callback_data: "postcard_photo" }],
+          [{ text: "Главное меню", callback_data: "main_menu" }],
+        ],
+      },
     });
   });
 
-  bot.action('postcard_text', async (ctx) => {
+  bot.action("postcard_text", async (ctx) => {
     try {
       await ctx.answerCbQuery();
     } catch (error: any) {
-      if (!error.description?.includes('query is too old')) {
-        console.error('Ошибка answerCbQuery:', error.message);
+      if (!error.description?.includes("query is too old")) {
+        console.error("Ошибка answerCbQuery:", error.message);
       }
     }
 
@@ -54,40 +70,69 @@ export function registerPostcardHandlers(bot: Telegraf<BotContext>, userStates: 
 
     try {
       await ctx.replyWithVideo(HERO_VIDEO_TEXT, {
-        parse_mode: 'HTML',
+        parse_mode: "HTML",
         caption: message,
         reply_markup: {
           inline_keyboard: [
-            [{text: '💌 Создать открытку', callback_data: 'postcard_text_start'}],
-            [{text: 'Видео-инструкция', callback_data: 'postcard_text_instruction'}],
-            [{text: '💳 Пополнить баланс', callback_data: 'refill_balance_from_postcard_text'}],
-            [{text: 'Назад', callback_data: 'postcard'}]
-          ]
-        }
-
-      })
+            [
+              {
+                text: "💌 Создать открытку",
+                callback_data: "postcard_text_start",
+              },
+            ],
+            [
+              {
+                text: "Видео-инструкция",
+                callback_data: "postcard_text_instruction",
+              },
+            ],
+            [
+              {
+                text: "💳 Пополнить баланс",
+                callback_data: "refill_balance_from_postcard_text",
+              },
+            ],
+            [{ text: "Назад", callback_data: "postcard" }],
+          ],
+        },
+      });
     } catch (error: any) {
       console.log(error);
       await ctx.reply(message, {
-      parse_mode: 'HTML',
-      reply_markup: {
-        inline_keyboard: [
-          [{text: '💌 Открытка из текста ', callback_data: 'postcard_text_start'}],
-          [{text: 'Видео-инструкция', callback_data: 'postcard_text_instruction'}],
-          [{text: '💳 Пополнить баланс', callback_data: 'refill_balance_from_postcard_text'}],
-          [{text: 'Назад', callback_data: 'postcard'}]
-        ]
-      }
-    })
+        parse_mode: "HTML",
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "💌 Открытка из текста ",
+                callback_data: "postcard_text_start",
+              },
+            ],
+            [
+              {
+                text: "Видео-инструкция",
+                callback_data: "postcard_text_instruction",
+              },
+            ],
+            [
+              {
+                text: "💳 Пополнить баланс",
+                callback_data: "refill_balance_from_postcard_text",
+              },
+            ],
+            [{ text: "Назад", callback_data: "postcard" }],
+          ],
+        },
+      });
     }
   });
 
-  bot.action('postcard_text_start', async (ctx) => {
+  bot.action("postcard_text_start", async (ctx) => {
     try {
       await ctx.answerCbQuery();
     } catch (error: any) {
-      if (!error.description?.includes('query is too old')) {
-        console.error('Ошибка answerCbQuery:', error.message);
+      if (!error.description?.includes("query is too old")) {
+        console.error("Ошибка answerCbQuery:", error.message);
       }
     }
 
@@ -98,17 +143,17 @@ export function registerPostcardHandlers(bot: Telegraf<BotContext>, userStates: 
 
     if (await Database.hasEnoughBalance(userId, PRICES.POSTCARD_TEXT)) {
       userStates.set(userId, {
-        step: 'waiting_postcard_text',
+        step: "waiting_postcard_text",
       });
 
       await ctx.reply(message, {
-        parse_mode: 'HTML',
+        parse_mode: "HTML",
         reply_markup: {
           inline_keyboard: [
-            [{text: 'Назад', callback_data: 'postcard_text'}]
-          ]
-        }
-      })
+            [{ text: "Назад", callback_data: "postcard_text" }],
+          ],
+        },
+      });
     } else {
       const balance = await Database.getUserBalance(userId);
 
@@ -121,69 +166,104 @@ export function registerPostcardHandlers(bot: Telegraf<BotContext>, userStates: 
 Чтобы продолжить, <b>пополните баланс</b>
 
 Выберите способ оплаты ⤵️`.trim();
-    
-    await ctx.telegram.sendMessage(userId, paymentMessage, {
-      parse_mode: 'HTML',
-      reply_markup: {
-        inline_keyboard: [
-          [{text: 'Оплата картой', callback_data: 'refill_balance_from_postcard_text'}],
-          [{text: 'Главное меню', callback_data: 'main_menu'}]
-        ]
-      }
-    });
+
+      await ctx.telegram.sendMessage(userId, paymentMessage, {
+        parse_mode: "HTML",
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Оплата картой",
+                callback_data: "refill_balance_from_postcard_text",
+              },
+            ],
+            [{ text: "Главное меню", callback_data: "main_menu" }],
+          ],
+        },
+      });
     }
   });
 
-  bot.action('postcard_photo', async (ctx) => {
+  bot.action("postcard_photo", async (ctx) => {
     try {
       await ctx.answerCbQuery();
     } catch (error: any) {
-      if (!error.description?.includes('query is too old')) {
-        console.error('Ошибка answerCbQuery:', error.message);
+      if (!error.description?.includes("query is too old")) {
+        console.error("Ошибка answerCbQuery:", error.message);
       }
     }
-    
+
     const userId = ctx.from?.id;
     if (!userId) return;
 
     const balance = await Database.getUserBalance(userId);
 
     const message = getPostcardPhotoMessage(balance);
-    
+
     try {
       await ctx.replyWithVideo(HERO_PHOTO_VIDEO_ID, {
-        parse_mode: 'HTML',
+        parse_mode: "HTML",
         caption: message,
         reply_markup: {
           inline_keyboard: [
-            [{text: '🏞 Создать открытку', callback_data: 'postcard_photo_start'}],
-            [{text: 'Видео-инструкция', callback_data: 'postcard_photo_instruction'}],
-            [{text: '💳 Пополнить баланс', callback_data: 'refill_balance_from_postcard_photo'}],
-            [{text: 'Назад', callback_data: 'postcard'}]
-          ]
-        }
-      })
-    } catch(error: any) {
+            [
+              {
+                text: "🏞 Создать открытку",
+                callback_data: "postcard_photo_start",
+              },
+            ],
+            [
+              {
+                text: "Видео-инструкция",
+                callback_data: "postcard_photo_instruction",
+              },
+            ],
+            [
+              {
+                text: "💳 Пополнить баланс",
+                callback_data: "refill_balance_from_postcard_photo",
+              },
+            ],
+            [{ text: "Назад", callback_data: "postcard" }],
+          ],
+        },
+      });
+    } catch (error: any) {
       await ctx.reply(message, {
-      parse_mode: 'HTML',
-      reply_markup: {
-        inline_keyboard: [
-          [{text: '🏞 Создать открытку  ', callback_data: 'postcard_photo_start'}],
-          [{text: 'Видео-инструкция', callback_data: 'postcard_photo_instruction'}],
-          [{text: '💳 Пополнить баланс', callback_data: 'refill_balance_from_postcard_photo'}],
-          [{text: 'Назад', callback_data: 'postcard'}]
-        ]
-      }
-    });
+        parse_mode: "HTML",
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "🏞 Создать открытку  ",
+                callback_data: "postcard_photo_start",
+              },
+            ],
+            [
+              {
+                text: "Видео-инструкция",
+                callback_data: "postcard_photo_instruction",
+              },
+            ],
+            [
+              {
+                text: "💳 Пополнить баланс",
+                callback_data: "refill_balance_from_postcard_photo",
+              },
+            ],
+            [{ text: "Назад", callback_data: "postcard" }],
+          ],
+        },
+      });
     }
-  })
+  });
 
-  bot.action('postcard_photo_start', async (ctx) => {
+  bot.action("postcard_photo_start", async (ctx) => {
     try {
       await ctx.answerCbQuery();
     } catch (error: any) {
-      if (!error.description?.includes('query is too old')) {
-        console.error('Ошибка answerCbQuery:', error.message);
+      if (!error.description?.includes("query is too old")) {
+        console.error("Ошибка answerCbQuery:", error.message);
       }
     }
 
@@ -191,74 +271,79 @@ export function registerPostcardHandlers(bot: Telegraf<BotContext>, userStates: 
     if (!userId) return;
 
     const message = POSTCARD_PHOTO_START;
-    
+
     if (await Database.hasEnoughBalance(userId, PRICES.POSTCARD_PHOTO)) {
       userStates.set(userId, {
-        step: 'waiting_postcard_photo',
+        step: "waiting_postcard_photo",
       });
       if (EXAMPLE_POSTCARD_PHOTO_ID && EXAMPLE_POSTCARD_PHOTO_ID.length > 0) {
         try {
           await ctx.replyWithPhoto(EXAMPLE_POSTCARD_PHOTO_ID, {
-          caption: message,
-          parse_mode: 'HTML',
-          reply_markup: {
-            inline_keyboard: [
-              [{text: 'Назад', callback_data: 'postcard_photo'}]
-            ]
-          }
-        });
+            caption: message,
+            parse_mode: "HTML",
+            reply_markup: {
+              inline_keyboard: [
+                [{ text: "Назад", callback_data: "postcard_photo" }],
+              ],
+            },
+          });
         } catch (error: any) {
           const messageWitoutExample = POSTCARD_PHOTO_START_WIHOUT;
           await ctx.reply(messageWitoutExample, {
-            parse_mode: 'HTML',
+            parse_mode: "HTML",
             reply_markup: {
               inline_keyboard: [
-                [{text: 'Назад', callback_data: 'postcard_photo'}]
-              ]
-            }
+                [{ text: "Назад", callback_data: "postcard_photo" }],
+              ],
+            },
           });
         }
         return;
       }
       await ctx.reply(message, {
-        parse_mode: 'HTML',
+        parse_mode: "HTML",
         reply_markup: {
           inline_keyboard: [
-            [{text: 'Назад', callback_data: 'postcard_photo'}]
-          ]
-        }
+            [{ text: "Назад", callback_data: "postcard_photo" }],
+          ],
+        },
       });
     } else {
       const balance = await Database.getUserBalance(userId);
 
-    const paymentMessage = `
+      const paymentMessage = `
 К сожалению, <b>на вашем балансе недостаточно средств</b> для создания генерации 😢
 
 <blockquote>💰 Ваш баланс: ${balance.toFixed(2)}₽
-🏞 Генерация 1 Открытки: ${PRICES.POSTCARD_TEXT.toFixed(2)}₽</blockquote>
+🏞 Генерация 1 Открытки: ${PRICES.POSTCARD_PHOTO.toFixed(2)}₽</blockquote>
 
 Чтобы продолжить, <b>пополните баланс</b>
 
 Выберите способ оплаты ⤵️`.trim();
-    
-    await ctx.telegram.sendMessage(userId, paymentMessage, {
-      parse_mode: 'HTML',
-      reply_markup: {
-        inline_keyboard: [
-          [{text: 'Оплата картой', callback_data: 'refill_balance_from_postcard_photo'}],
-          [{text: 'Главное меню', callback_data: 'main_menu'}]
-        ]
-      }
-    });
+
+      await ctx.telegram.sendMessage(userId, paymentMessage, {
+        parse_mode: "HTML",
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Оплата картой",
+                callback_data: "refill_balance_from_postcard_photo",
+              },
+            ],
+            [{ text: "Главное меню", callback_data: "main_menu" }],
+          ],
+        },
+      });
     }
   });
 
-  bot.action('postcard_text_instruction', async (ctx) => {
+  bot.action("postcard_text_instruction", async (ctx) => {
     try {
       await ctx.answerCbQuery();
     } catch (error: any) {
-      if (!error.description?.includes('query is too old')) {
-        console.error('Ошибка answerCbQuery:', error.message);
+      if (!error.description?.includes("query is too old")) {
+        console.error("Ошибка answerCbQuery:", error.message);
       }
     }
 
@@ -268,36 +353,36 @@ export function registerPostcardHandlers(bot: Telegraf<BotContext>, userStates: 
     const messge = `
 📹 <b>Видео-инструкция по созданию открыток из текста</b>
 
-Смотрите короткое видео, чтобы правильно и качественно выполнять шаги и получать потрясающие результаты 🔥`.trim()
+Смотрите короткое видео, чтобы правильно и качественно выполнять шаги и получать потрясающие результаты 🔥`.trim();
 
     try {
       await ctx.replyWithVideo(POSTCARD_TEXT_INSTRUCTION, {
         caption: messge,
-        parse_mode: 'HTML',
+        parse_mode: "HTML",
         reply_markup: {
           inline_keyboard: [
-            [{text: 'Назад', callback_data: 'postcard_text'}]
-          ]
-        }
+            [{ text: "Назад", callback_data: "postcard_text" }],
+          ],
+        },
       });
     } catch (error: any) {
-      await ctx.reply('Ошибка воспроизведения видео!', {
-        parse_mode: 'HTML',
+      await ctx.reply("Ошибка воспроизведения видео!", {
+        parse_mode: "HTML",
         reply_markup: {
           inline_keyboard: [
-            [{text: 'Назад', callback_data: 'postcard_text'}]
-          ]
-        }
-      })
+            [{ text: "Назад", callback_data: "postcard_text" }],
+          ],
+        },
+      });
     }
   });
 
-  bot.action('postcard_photo_instruction', async (ctx) => {
+  bot.action("postcard_photo_instruction", async (ctx) => {
     try {
       await ctx.answerCbQuery();
     } catch (error: any) {
-      if (!error.description?.includes('query is too old')) {
-        console.error('Ошибка answerCbQuery:', error.message);
+      if (!error.description?.includes("query is too old")) {
+        console.error("Ошибка answerCbQuery:", error.message);
       }
     }
 
@@ -307,27 +392,27 @@ export function registerPostcardHandlers(bot: Telegraf<BotContext>, userStates: 
     const messge = `
 📹 <b>Видео-инструкция по созданию открыток из вашего фото</b>
 
-Смотрите короткое видео, чтобы правильно и качественно выполнять шаги и получать потрясающие результаты 🔥`.trim()
+Смотрите короткое видео, чтобы правильно и качественно выполнять шаги и получать потрясающие результаты 🔥`.trim();
 
     try {
       await ctx.replyWithVideo(POSTCARD_PHOTO_INSTRUCTION, {
         caption: messge,
-        parse_mode: 'HTML',
+        parse_mode: "HTML",
         reply_markup: {
           inline_keyboard: [
-            [{text: 'Назад', callback_data: 'postcard_text'}]
-          ]
-        }
+            [{ text: "Назад", callback_data: "postcard_text" }],
+          ],
+        },
       });
     } catch (error: any) {
-      await ctx.reply('Ошибка воспроизведения видео!', {
-        parse_mode: 'HTML',
+      await ctx.reply("Ошибка воспроизведения видео!", {
+        parse_mode: "HTML",
         reply_markup: {
           inline_keyboard: [
-            [{text: 'Назад', callback_data: 'postcard_photo'}]
-          ]
-        }
-      })
+            [{ text: "Назад", callback_data: "postcard_photo" }],
+          ],
+        },
+      });
     }
   });
 }
